2a51d31baa01e3a5f1291d2a2c503bee1d548241
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-24 15:45:41 +0300

    Add counter, start todo

--- src/cljs/reagent_examples/core.cljs
+++ src/cljs/reagent_examples/core.cljs
@@ -1,19 +1,58 @@
 (ns reagent-examples.core
-    (:require [reagent.core :as reagent :refer [atom]]
+    (:require [reagent.core :as r :refer [atom]]
               [reagent.session :as session]
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
 
 ;; -------------------------
+
+(defonce next-id (atom 1))
+
+;; -------------------------
+
+(defonce counter (atom 0))
+(defn up [] (swap! counter inc))
+(defn down [] (swap! counter dec))
+
+;; -------------------------
+
+(defonce todos (atom (sorted-map)))
+
+(defn todo-next-id [] (swap! next-id inc))
+
+(defn todo-add [text]
+  (let [id (todo-next-id)
+        todo {:id id :text text :completed false}]
+    (swap! todos assoc id todo)))
+
+(defn todo-toggle [id]
+  (swap! todos update-in [id :completed] not))
+
+(defn todo-edit [id title]
+  (swap! todos assoc-in [id :title]))
+
+;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to reagent-examples"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+  [:div [:h2 "index"]
+   [:ul
+    [:li [:a {:href "/counter"} "counter"]]
+    [:li [:a {:href "/todo"} "todo"]]]])
 
-(defn about-page []
-  [:div [:h2 "About reagent-examples"]
-   [:div [:a {:href "/"} "go to the home page"]]])
+(defn counter-page []
+  [:div [:h2 "counter"]
+   [:div [:a {:href "/"} "index"]]
+   [:div
+    [:h3 @counter]
+    [:button {:on-click up} "up"]
+    [:button {:on-click down} "down"]]])
+
+(defn todo-page []
+  [:div [:h2 "todo"]
+   [:div [:a {:href "/"} "index"]
+    [:div
+     [:h3 "lol"]]]])
 
 (defn current-page []
   [:div [(session/get :current-page)]])
@@ -24,14 +63,17 @@
 (secretary/defroute "/" []
   (session/put! :current-page #'home-page))
 
-(secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
+(secretary/defroute "/counter" []
+  (session/put! :current-page #'counter-page))
+
+(secretary/defroute "/todo" []
+  (session/put! :current-page #'todo-page))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
+  (r/render [current-page] (.getElementById js/document "app")))
 
 (defn init! []
   (accountant/configure-navigation!)
