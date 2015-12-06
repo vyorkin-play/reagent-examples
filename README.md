0fa6929345c1e82e2110e71b9abc43c3c833ed22
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-12-06 17:37:11 +0300

    :octocat:

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Just experimenting with [reagent](http://reagent-project.github.io/), nothing se
 * Twitter: [@ReagentProject](https://twitter.com/reagentproject)
 * YouTube: [Reagent Project](https://www.youtube.com/channel/UC1UP5LiNNNf0a45dA9eDA0Q)
 * [Google Groups mailing list](https://groups.google.com/forum/#!forum/reagent-project)
+* [cljs cheat sheet #1](https://github.com/readevalprintlove/clojurescript-cheatsheet/blob/master/cljs-cheatsheet.pdf)
+* [cljs cheat sheet #2](http://cljs.info/cheatsheet/)
 
 # Examples
 
--- src/cljs/reagent_examples/todo/component.cljs
+++ src/cljs/reagent_examples/todo/component.cljs
@@ -1,23 +1,58 @@
 (ns reagent-examples.todo.component
-  (:require [reagent.core :as r :refer [atom]]))
+  (:require [reagent.core :as r :refer [atom]]
+            [clojure.string :as s :refer [trim]]))
 
 (defonce todos (atom (sorted-map)))
 (defonce counter (atom 0))
 
-(defn add-todo [text]
+(def key-enter 13)
+(def key-escape 27)
+
+(defn create-todo [title]
   (let [id (swap! counter inc)]
-    (swap! todos assoc id {:id id :title text :done false})))
+    (swap! todos assoc id {:id id :title title :done false})))
 
-(defonce init (do
-               (add-todo "One")
-               (add-todo "Two")))
+(defn update-todo [id title]
+  (swap! todos assoc-in [id :title] title))
 
-(defn todo-input []
-  [:div "input"])
+(defonce init
+  (do
+   (create-todo "learn some shit")
+   (create-todo "write some code")
+   (create-todo "die")))
+
+(defn clear-todos []
+  (reset! todos ))
+
+(defn input-render [{:keys [title on-save]}]
+  (let [val (r/atom title)
+        save #(let [v (-> @val str trim)]
+                (when-not (empty? v) (on-save v)))
+        change #(reset! val %)
+        key-down #(condp = %
+                    key-enter (save)
+                    nil)]
+    (fn [props]
+      [:input (merge props
+                     {:type "text"
+                      :value @val
+                      :on-change #(change (-> % .-target .-value))
+                      :on-key-down #(key-down (.-which %))})])))
+
+(defn input-did-mount [c]
+  (.focus (r/dom-node c)))
+
+(defn input-component []
+  (r/create-class {:reagent-render input-render
+                   :component-did-mount input-did-mount}))
+
+(defn todo-item [{:keys [id title]}]
+  ^{:key id} [:li title])
 
 (defn component []
   [:div
-   (todo-input)
+   [input-component {:on-save create-todo
+                :placeholder "What needs to be done?"}]
    [:ul.todos
     (for [todo (vals @todos)]
-      ^{:key (:id todo)} [:li (:title todo)])]])
+      (todo-item todo))]])
